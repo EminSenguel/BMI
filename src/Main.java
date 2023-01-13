fcc5d7963102a68b2fb070c60e0212616d81afa1
EminS <wi22236@lehre.dhbw-stuttgart.de>
2023-01-13 13:55:35 +0100

    Neu mit Daten

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     static int groeße;
 
     static double gewicht;
+    static char infotxt;
 
 
     public static void main(String[] args) {
@@ -44,22 +45,54 @@ public class Main {
     }
 
     public static String outcome(double input)
-    {
+    { Scanner myScanner = new Scanner(System.in);
         String output = null;
 
         if
         (input >= 25) {
             output = "Astronomer Adolphe Quetelet says overweight";
             System.out.println(output);
-        } else if (input >= 18.5) {
+            System.out.println("Do you want more information?");
+            infotxt = myScanner.next().charAt(0);
+
+            if (infotxt == 'y')
+                System.out.println("");
+                else {
+                    System.out.println("OK! See you!");
+                }
+            }
+
+            else if (input >= 18.5) {
             output = "Astronomer Adolphe Quetelet says standardweight";
             System.out.println(output);
-        } else if (input >= 15) {
+            System.out.println("Do you want more information?");
+            infotxt = myScanner.next().charAt(0);
+            if (infotxt == 'y')
+                System.out.println("");
+            else{
+                System.out.println("OK! See you!");
+                }
+
+            } else if (input >= 15) {
             output = "Astronomer Adolphe Quetelet says underweight";
             System.out.println(output);
+            System.out.println("Do you want more information?");
+            infotxt = myScanner.next().charAt(0);
+                if (infotxt == 'y')
+                    System.out.println("");
+                else {
+                        System.out.println("OK! See you!");
+                    }
         } else if (input < 15){
             output="You need to see a doctor  NOW!";
             System.out.println(output);
+            System.out.println("Do you want more information?");
+            infotxt = myScanner.next().charAt(0);
+                    if (infotxt == 'y')
+                        System.out.println("");
+                else {
+                            System.out.println("OK! See you!");
+                        }
         }
         return output;
     }
